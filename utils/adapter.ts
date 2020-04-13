ab36c920fb3788586800317c47458ba20adbfc13
Haoqun Jiang <haoqunjiang@gmail.com>
2020-04-13 17:31:57 +0800

    chore: remove unused adapter file

--- utils/adapter.ts
+++
@@ -1,7 +0,0 @@
-// @ts-ignore
-import adapt from 'vue-jscodeshift-adapter'
-
-// TODO:
-// 1. support lang="ts" block in .vue files
-// 2. automatically apply the adapter function to all transform modules
-export default adapt

b131b9ad073adb7ffd509cce8bcf9ad38fa72d11
Haoqun Jiang <haoqunjiang@gmail.com>
2020-04-29 15:45:14 +0800

    feat: implement new-vue-to-create-app

---
+++ transformations/__tests__/new-vue-to-create-app-test.ts
@@ -0,0 +1,66 @@
+import { defineInlineTest } from 'jscodeshift/src/testUtils'
+const transform = require('../new-vue-to-create-app')
+
+defineInlineTest(
+  transform,
+  {},
+  `new Vue({ template: "<div>hello</div>" })`,
+  `Vue.createApp({ template: "<div>hello</div>" })`,
+  'transform `new Vue()` to createApp()'
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `new Vue({ render: h => h(App) }).$mount("#app")`,
+  `Vue.createApp({ render: h => h(App) }).mount("#app")`,
+  'transform `new Vue().$mount` with options to createApp().mount'
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `var vm = new Vue({ template: "<div>hello</div>" }); vm.$mount("#app")`,
+  `var vm = Vue.createApp({ template: "<div>hello</div>" }); vm.mount("#app")`,
+  'transform `vm.$mount` to vm.mount'
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `new MyComponent().$mount("#app")`,
+  `Vue.createApp(MyComponent).mount("#app")`,
+  'transform `new MyComponent().$mount`'
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `new MyComponent({ foo: "bar" }).$mount("#app")`,
+  `Vue.createApp(MyComponent, { foo: "bar" }).mount("#app")`,
+  'transform `new MyComponent().$mount` with additional options'
+)
+
+defineInlineTest(
+  transform,
+  { includeMaybeComponents: false },
+  `new MyComponent().$mount("#app"); vm.$mount("#app")`,
+  `new MyComponent().$mount("#app"); vm.$mount("#app")`,
+  'do not transform `new MyComponent().$mount` or `vm.$mount` if `includeMaybeComponents` disabled'
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `new Vue({ el: "#app", render: h => h(App) })`,
+  `Vue.createApp({\n  render: h => h(App)\n}).mount("#app")`,
+  'transform `new Vue` with `el` prop'
+)
+
+defineInlineTest(
+  transform,
+  {},
+  `new MyComponent({ el: "#app" })`,
+  `Vue.createApp(MyComponent).mount("#app")`,
+  'transform `new MyComponent` with `el` prop'
+)
---
+++ transformations/new-vue-to-create-app.ts
@@ -0,0 +1,183 @@
+import wrap from '../src/wrap-ast-transformation'
+import type { ASTTransformation } from '../src/wrap-ast-transformation'
+
+import type * as N from 'jscodeshift'
+
+type Params = {
+  // if false, do not take expressions like `new HelloWorld().$mount` or
+  // `new HelloWorld({ el: '#app' })`into account
+  includeMaybeComponents?: boolean
+}
+
+// Limitations: cannot transform expressions like `new HelloWorld()`
+// FIXME: need to ensure there will be a Vue import if needed.
+export const transformAST: ASTTransformation<Params | void> = (
+  context,
+  params: Params = {
+    includeMaybeComponents: true,
+  }
+) => {
+  const { j, root } = context
+  const { includeMaybeComponents = true } = params
+
+  const newVue = root.find(j.NewExpression, {
+    callee: {
+      type: 'Identifier',
+      name: 'Vue',
+    },
+  })
+
+  // new Vue() -> Vue.createApp()
+  newVue.replaceWith(({ node }) => {
+    const rootProps = node.arguments[0]
+    return j.callExpression(
+      j.memberExpression(j.identifier('Vue'), j.identifier('createApp')),
+      [rootProps]
+    )
+  })
+
+  const vueCreateApp = newVue
+  // Vue.createApp().$mount() -> Vue.createApp().mount()
+  vueCreateApp.forEach((path) => {
+    const parentNode = path.parent.node
+    if (
+      j.MemberExpression.check(parentNode) &&
+      parentNode.object === path.node &&
+      j.Identifier.check(parentNode.property) &&
+      parentNode.property.name === '$mount'
+    ) {
+      parentNode.property.name = 'mount'
+    }
+  })
+
+  // Vue.createApp({ el: '#app' }) -> Vue.createApp().mount('#app')
+  vueCreateApp.replaceWith(({ node }) => {
+    if (
+      node.arguments.length !== 1 ||
+      !j.ObjectExpression.check(node.arguments[0])
+    ) {
+      return node
+    }
+
+    const rootProps = node.arguments[0]
+    const elIndex = rootProps.properties.findIndex(
+      (p) =>
+        j.ObjectProperty.check(p) &&
+        j.Identifier.check(p.key) &&
+        p.key.name === 'el'
+    )
+
+    if (elIndex === -1) {
+      return node
+    }
+
+    const elProperty = rootProps.properties.splice(
+      elIndex,
+      1
+    )[0] as N.ObjectProperty
+    const elExpr = elProperty.value
+    return j.callExpression(
+      j.memberExpression(node, j.identifier('mount')),
+      // @ts-ignore I'm not sure what the edge cases are
+      [elExpr]
+    )
+  })
+
+  if (includeMaybeComponents) {
+    // new My().$mount
+    const new$mount = root.find(j.CallExpression, (n: N.CallExpression) => {
+      return (
+        j.MemberExpression.check(n.callee) &&
+        j.NewExpression.check(n.callee.object) &&
+        j.Identifier.check(n.callee.property) &&
+        n.callee.property.name === '$mount'
+      )
+    })
+    new$mount.replaceWith(({ node }) => {
+      const el = node.arguments[0]
+
+      const instance = (node.callee as N.MemberExpression)
+        .object as N.NewExpression
+      const ctor = instance.callee
+
+      return j.callExpression(
+        j.memberExpression(
+          j.callExpression(
+            j.memberExpression(j.identifier('Vue'), j.identifier('createApp')),
+            [
+              ctor,
+              ...instance.arguments, // additional props
+            ]
+          ),
+          j.identifier('mount')
+        ),
+        [el]
+      )
+    })
+
+    // vm.$mount
+    const $mount = root.find(j.CallExpression, {
+      callee: {
+        type: 'MemberExpression',
+        property: {
+          type: 'Identifier',
+          name: '$mount',
+        },
+      },
+      arguments: (args: Array<any>) => args.length === 1,
+    })
+    $mount.forEach(({ node }) => {
+      // @ts-ignore
+      node.callee.property.name = 'mount'
+    })
+
+    // new My({ el })
+    const newWithEl = root.find(j.NewExpression, (n: N.NewExpression) => {
+      return (
+        n.arguments.length === 1 &&
+        j.ObjectExpression.check(n.arguments[0]) &&
+        n.arguments[0].properties.some(
+          (prop) =>
+            j.ObjectProperty.check(prop) &&
+            j.Identifier.check(prop.key) &&
+            prop.key.name === 'el'
+        )
+      )
+    })
+
+    newWithEl.replaceWith(({ node }) => {
+      const rootProps = node.arguments[0] as N.ObjectExpression
+      const elIndex = rootProps.properties.findIndex(
+        (p) =>
+          j.ObjectProperty.check(p) &&
+          j.Identifier.check(p.key) &&
+          p.key.name === 'el'
+      )
+      const elProperty = rootProps.properties.splice(
+        elIndex,
+        1
+      )[0] as N.ObjectProperty
+      const elExpr = elProperty.value
+
+      const ctor = node.callee
+      return j.callExpression(
+        j.memberExpression(
+          j.callExpression(
+            j.memberExpression(j.identifier('Vue'), j.identifier('createApp')),
+            [
+              ctor,
+              // additional props, and skip empty objects
+              ...(rootProps.properties.length > 0 ? [rootProps] : []),
+            ]
+          ),
+          j.identifier('mount')
+        ),
+        // @ts-ignore I'm not sure what the edge cases are
+        [elExpr]
+      )
+    })
+  }
+}
+
+export default wrap(transformAST)
+export const parser = 'babylon'

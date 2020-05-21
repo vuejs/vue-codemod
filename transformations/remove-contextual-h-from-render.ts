3ad801ee225f66cb2ffafe266c2227ac3ce39810
Haoqun Jiang <haoqunjiang@gmail.com>
2020-05-22 01:19:57 +0800

    feat: support removing `h` from render methods

--- transformations/remove-contextual-h-from-render.ts
+++ transformations/remove-contextual-h-from-render.ts
@@ -16,7 +16,15 @@ export const transformAST: ASTTransformation = (context) => {
     },
   })
 
-  if (renderFns.length) {
+  const renderMethods = root.find(j.ObjectMethod, {
+    key: {
+      name: 'render',
+    },
+    params: (params: Array<any>) =>
+      j.Identifier.check(params[0]) && params[0].name === 'h',
+  })
+
+  if (renderFns.length || renderMethods.length) {
     addImport(context, {
       specifier: { type: 'named', imported: 'h' },
       source: 'vue',
@@ -25,9 +33,11 @@ export const transformAST: ASTTransformation = (context) => {
     renderFns.forEach(({ node }) => {
       ;(node.value as ArrowFunctionExpression).params.shift()
     })
-  }
 
-  // TODO: render methods
+    renderMethods.forEach(({ node }) => {
+      node.params.shift()
+    })
+  }
 }
 
 export default wrap(transformAST)

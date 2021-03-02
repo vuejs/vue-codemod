e5d652f34e9d7aec59f15e3282280b2cfee0f272
kazuya kawaguchi <kawakazu80@gmail.com>
2021-03-02 22:16:49 +0900

    chore(playground): upgrade vite (#5)

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
 const API_PORT = process.env.API_PORT || 3002
 
-export default {
-  proxy: {
-    '/api': {
-      target: `http://localhost:${API_PORT}/`,
-      changeOrigin: true,
-      rewrite: (path: string) => path.replace(/^\/api/, '')
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${API_PORT}/`,
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, '')
+      }
     }
-  }
-}
+  },
+  plugins: [
+    vue()
+  ]
+})

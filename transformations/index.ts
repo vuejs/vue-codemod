ad1ce112e2c8071a21520173d41cc9cf5d3719d6
ygj6 <yuangongji@foxmail.com>
2021-06-15 16:13:08 +0800

    feat: implement global-filter transformation

--- transformations/index.ts
+++ transformations/index.ts
@@ -19,6 +19,7 @@ const transformationMap: {
   'remove-vue-set-and-delete': require('./remove-vue-set-and-delete'),
   'rename-lifecycle': require('./rename-lifecycle'),
   'add-emit-declaration': require('./add-emit-declaration'),
+  'global-filter': require('./global-filter'),
 
   // atomic ones
   'remove-contextual-h-from-render': require('./remove-contextual-h-from-render'),

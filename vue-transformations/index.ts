182fc2092b75ae57e22d5f140046ddce9d7b0724
ygj6 <yuangongji@foxmail.com>
2021-06-19 14:22:57 +0800

    chore: add slot-scope-attribute transformation to index.ts

--- vue-transformations/index.ts
+++ vue-transformations/index.ts
@@ -9,6 +9,7 @@ const transformationMap: {
 } = {
   'slot-attribute': require('./slot-attribute'),
   'slot-default': require('./slot-default'),
+  'slot-scope-attribute': require('./slot-scope-attribute'),
   'v-for-template-key': require('./v-for-template-key'),
   'v-else-if-key': require('./v-else-if-key'),
   'transition-group-root': require('./transition-group-root'),
